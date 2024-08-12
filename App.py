2cc4f3a3494e7377e0e1a7f84d551c59dbb5e2e3
sunray4 <118241363+sunray4@users.noreply.github.com>
2024-08-11 20:26:52 -0700

    Update App.py

--- App.py
+++ App.py
@@ -1,3 +1,13 @@
-for i in range(0,5):
-  print("HELLO!")
-print("angelina sucks")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Add more routes for other pages if necessary
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
